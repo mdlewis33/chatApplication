655b7e19d2faa54f86500164ff014508cfb4a340
mdlewis33 <114125152+mdlewis33@users.noreply.github.com>
2023-04-20 21:36:34 -0400

    Add files via upload

--- chatApplication_Client.py
+++ chatApplication_Client.py
@@ -2,27 +2,13 @@ import socket
 import time
 import re
 
-def init_client_ip():
-    # Regular expression statement
-    regex = "^([0-9]{1,3}\.)([0-9]{1,3}\.){2}([0-9]{1,3})$"
-
-    # Set the server IP address and checks to see if the IP address is valid
-    valid = False
-    while not valid:
-        global client_ip
-        client_ip = input("Enter the server IP address (<Enter> for localhost): ")
-        if client_ip == '':
-            client_ip = '127.0.0.1'
-
-            if re.search(regex, client_ip):
-                valid = True
-
 def init_client_port():
     # Set the client port number
     global client_port
     client_port = input("Enter the server port number (<Enter> for localhost): ")
     if client_port == '':
         client_port = 8000
+    init_client_username()
 
 def init_client_username():
     # Set the client username
@@ -30,6 +16,7 @@ def init_client_username():
     client_username = input("Enter your username (<Enter> for user): ")
     if client_username == '':
         client_username = 'user'
+    init_client_connect()
 
 def init_client_connect():
     # Create client socket using TCP
@@ -49,45 +36,46 @@ def init_client_connect():
     time.sleep(1)
     print("Connection established.")
 
-# Loop that continues the chat between the server and the client(this file) until either inputs "end"
-while True:
-    # Initializes client message for sending to server
-    client_message = client_username + "."
 
-    # Get user message to send to the client
-    client_message_input = input("Enter a message to send to the server (Type end to stop): ").strip()
+    # Loop that continues the chat between the server and the client(this file) until either inputs "end"
+    while True:
+        # Initializes client message for sending to server
+        client_message = client_username + "."
 
-    # Checks if the inputted message is empty and if so, the client has to input until the message is not empty
-    while client_message_input == "":
-        print("Invalid Input.")
+        # Get user message to send to the client
         client_message_input = input("Enter a message to send to the server (Type end to stop): ").strip()
 
-    # Stops the loop if the user inputs end
-    if client_message_input.lower() == "end":
-        break
+        # Checks if the inputted message is empty and if so, the client has to input until the message is not empty
+        while client_message_input == "":
+            print("Invalid Input.")
+            client_message_input = input("Enter a message to send to the server (Type end to stop): ").strip()
 
-    # Adds the input message to the previously prepared message to send to server
-    client_message += client_message_input
+        # Stops the loop if the user inputs end
+        if client_message_input.lower() == "end":
+            break
 
-    # Prints the inputted message to the screen
-    print("<" + time.asctime(time.localtime()) + "> " + "[" + client_username + "]: " + client_message_input)
+        # Adds the input message to the previously prepared message to send to server
+        client_message += client_message_input
 
-    # Sends message to the server
-    client_socket.send(client_message.encode())
+        # Prints the inputted message to the screen
+        print("<" + time.asctime(time.localtime()) + "> " + "[" + client_username + "]: " + client_message_input)
 
-    # Waits and receives new message from the server
-    server_message = client_socket.recv(2048).decode()
+        # Sends message to the server
+        client_socket.send(client_message.encode())
 
-    # Splits the inputted string into the username and received message
-    server_username = server_message.split(".")[0]
-    server_message = server_message.split(".")[1]
+        # Waits and receives new message from the server
+        server_message = client_socket.recv(2048).decode()
 
-    # Checks to see if the received message was end and if so, ends the loop
-    if server_message.lower() == "end":
-        break
+        # Splits the inputted string into the username and received message
+        server_username = server_message.split(".")[0]
+        server_message = server_message.split(".")[1]
 
-    # Prints the received message to the screen with the servers username
-    print("<" + time.asctime(time.localtime()) + "> " + "[" + server_username + "]: " + server_message)
+        # Checks to see if the received message was end and if so, ends the loop
+        if server_message.lower() == "end":
+            break
+
+        # Prints the received message to the screen with the servers username
+        print("<" + time.asctime(time.localtime()) + "> " + "[" + server_username + "]: " + server_message)
 
 def client_end():
     # When the loop ends checks if the client or server ended the chat and responds accordingly
@@ -101,3 +89,18 @@ def client_end():
 
     # Closes the client socket
     client_socket.close()
+
+# Regular expression statement
+regex = "^([0-9]{1,3}\.)([0-9]{1,3}\.){2}([0-9]{1,3})$"
+
+# Set the server IP address and checks to see if the IP address is valid
+valid = False
+while not valid:
+    global client_ip
+    client_ip = input("Enter the server IP address (<Enter> for localhost): ")
+    if client_ip == '':
+        client_ip = '127.0.0.1'
+    if re.search(regex, client_ip):
+            valid = True
+
+init_client_port()
--- chatApplication_Server.py
+++ chatApplication_Server.py
@@ -9,74 +9,87 @@ def server_setup(serverFrame):
     global server_user_entry
     global server_ip_entry
 
-    # Create a label and an entry widget for the server IP address
+    # Creates a label widget for the server IP address
     server_ip_label = Label(serverFrame, text='Enter the server IP address (Leave blank for localhost):')
     server_ip_label.pack(padx=5, pady=5, side=TOP)
 
+    # Creates a frame for the server IP entry and button
     ip_entry_frame = Frame(serverFrame)
     ip_entry_frame.pack(padx=5, pady=5, side=TOP)
 
+    # Creates an entry widget for the server IP address
     server_ip_entry = Entry(ip_entry_frame)
     server_ip_entry.pack(side=LEFT)
 
-
+    # Creates a label widget for the server port
     server_port_label = Label(serverFrame, text='Enter the server port number (Leave blank for localhost): ')
     server_port_label.pack(padx=5, pady=5, side=TOP)
 
+    # Creates a frame for the server ports entry and button
     port_entry_frame = Frame(serverFrame)
     port_entry_frame.pack(padx=5, pady=5, side=TOP)
 
+    # Creates an entry widget for the server port address
     server_port_entry = Entry(port_entry_frame)
     server_port_entry.pack(side=LEFT)
 
-
+    # Creates a label widget for the server username
     server_user_label = Label(serverFrame, text='Enter your username (Leave blank for server): ')
     server_user_label.pack(padx=5, pady=5, side=TOP)
 
+    # Creates a frame for the server usernames entry and button
     user_entry_frame = Frame(serverFrame)
     user_entry_frame.pack(padx=5, pady=5, side=TOP)
 
+    # Creates an entry widget for the server port
     server_user_entry = Entry(user_entry_frame)
     server_user_entry.pack(side=LEFT)
 
+    # Creates a button that when clicked starts the on_ok button function
     okBtn = Button(serverFrame, text='OK', bd=3, command=lambda: on_ok(serverFrame))
     okBtn.pack(side=BOTTOM)
 
+    # Keeps the server frame alive until later destroyed/cleared
     serverFrame.mainloop()
 
 def on_ok(serverFrame):
-    # Regular expression statement
+    # Regular expression statement to test
     regex = "^([0-9]{1,3}\.)([0-9]{1,3}\.){2}([0-9]{1,3})$"
 
-    global server_ip
+    # Gets the inputted server ip from the server_ip_entry entry box
     server_ip = server_ip_entry.get()
+
+    # Checks the server ip to see if it's empty, if so sets it to the default ip address (127.0.0.1)
     if server_ip == '':
         server_ip = '127.0.0.1'
+    # Checks if the ip address matches the regular expression, if not creates a popup saying the ip was invalid
     if re.search(regex, server_ip):
-        uiGen.display_txt(serverFrame, server_ip)
+        # uiGen.display_txt(serverFrame, server_ip)
     else:
         uiGen.show_popup("Invalid IP Address.")
 
-    global server_port
+    # Gets the inputted server port from the server_port_entry entry box
     server_port = server_port_entry.get()
+
+    # Checks if the server port is empty, if so sets it to the default port number (8000)
     if server_port == '':
         server_port = 8000
-    uiGen.display_txt(serverFrame, server_port)
+    #uiGen.display_txt(serverFrame, server_port)
 
-    global server_username
+    # Gets the inputted server username from the server_user_entry
     server_username = server_user_entry.get()
+
+    # Checks if the server username is empty, if so sets it to the default server username (server)
     if server_username == '':
         server_username = 'server'
     uiGen.display_txt(serverFrame, server_username)
 
-    #get_setup(serverFrame)
+    init_server_connect(serverFrame, server_ip, server_port)
 
-def get_setup(serverFrame):
+
+def init_server_connect(serverFrame, server_ip, server_port):
     uiGen.clear_frame(serverFrame)
 
-
-
-def init_server_connect():
     # Create server socket
     server_socket = socket(AF_INET, SOCK_STREAM)
 
@@ -86,7 +99,10 @@ def init_server_connect():
     # Listen for incoming connections
     server_socket.listen()
 
-    print("Trying to connect to another user...")
+    connect_label = Label(serverFrame, text='Trying to connect to another user...')
+    connect_label.pack(padx=5, pady=5)
+
+    serverFrame.update()
 
     # Create connection socket
     connection_socket, address = server_socket.accept()
@@ -94,8 +110,14 @@ def init_server_connect():
     # Receive the client message
     client_message = connection_socket.recv(2048).decode()
 
-    print("Connection established.")
+    connect_label.config(text='Connection established.')
 
+    message_label = Label(serverFrame, text=f'Received message: {client_message}')
+    message_label.pack(padx=5, pady=5)
+
+    serverFrame.update()
+
+def main_loop():
     # Loop that continues the chat between the server(this file) and the client until either inputs "end"
     while True:
         # Initializes server message for sending to client
